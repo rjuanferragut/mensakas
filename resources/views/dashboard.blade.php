85820ad8d291964d4dd6222830b6a4c1a0ad2e06
rafa <rjuanferragut@gmail.com>
2020-02-13 14:38:41 +0100

    arreglos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,10 @@
   <div>
     <h1 class="mt-5 text-center"> Bienvenido al panel del administrador</h1>
   </div>
+  <div class="mt-5">
+    <h3>Panel de información general</h3>
+
+  </div>
   <!-- información general de las categorias -->
 </div>
 @endsection
