00da9a5aedff1db839e9cfda67275bfec5c38dff
Ferran Herrero <ferran.herrerosoto@gmail.com>
2020-02-13 03:02:23 +0100

    Insert creation

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use DB;
 
 class CustomersController extends Controller
@@ -30,62 +31,46 @@ class CustomersController extends Controller
     }
 
     public function store(Request $request) {
+      // dd($request);
       $validate = $request->validate([
-          'customer_name' => 'required',
-          'customer_last_name' => 'required',
-          'customer_phone' => 'required',
-          'customer_password' => 'required',
-          'customer_email' => 'required',
-          'customer_address' => 'required',
-          'customer_zip' => 'required'
+          'name' => 'required',
+          'last_name' => 'required',
+          'phone' => 'required',
+          'password' => 'required',
+          'email' => 'required',
+          'address' => 'required',
+          'state' => 'required',
+          'country' => 'required',
+          'zip' => 'required'
       ]);
+      // dd($request);
 
-      if($validate){
-        dd($validate);
-        // $country = DB::table('country')->select('id_country')->where()
-        //
-        // DB::table('address')->insert([
-        //
-        // ]);
-        //
-        //   DB::table('customers')->insert([
-        //       'firstname' => $request['firstname'],
-        //       'lastname' => $request['lastname'],
-        //       'email' => $request['email'],
-        //       'passwd' => Hash::make($request['siret']),
-        //       'siret' => $request['siret'],
-        //       'phone' => $request['phone'],
-        //       'phone_default' => $request['phone'],
-        //       'id_gender' => $request['gender'],
-        //       'company' => $request['company'],
-        //       'id_lang' => 1,
-        //       'id_shop' => 1,
-        //       'id_shop_group' => 1,
-        //       'id_default_group' => $request['group'],
-        //       'active' => 0,
-        //       'date_add' => Carbon::now(),
-        //       'date_upd' => Carbon::now()
-        //   ]);
-        //
-        //   $id = DB::getPdo()->lastInsertId();
-        //
-        //   DB::table('ps_customer_group')->insert([
-        //       'id_customer' => $id,
-        //       'id_group' => $request['group']
-        //   ]);
-        //
-        //   $data = array('name'=> $request['firstname'] . " " . $request['lastname'], 'email' => $request['email'], 'siret' => $request['siret'], 'phone' => $request['phone'], 'company' => $request['company']);
-        //
-        //   Mail::send('mail', $data, function($message) use ($data) {
-        //      $message->to($data['email'], $data['name'])->subject
-        //         ('[INFO] - Alta de Usuario en Castelao S.L.');
-        //      $message->from('castelaosl@castelaosl.com','Castelao S.L.');
-        //   });
+      // if($validate){
+        DB::table('address')->insertGetId(
+            ['id_country' => $request->country],
+            ['id_state' => $request->state],
+            ['address' => $request->address],
+            ['zipcode' => $request->zip],
+            ['active' => 1]
+        );
 
-          return redirect()->action('CustomersController@index');
-      }else{
-          return redirect()->back()->with('error', 'Faltan datos del cliente');
-      }
+        $addressInserted = DB::table('address')->where('id_state', $request->state)->where('address', $request->address)->where('active', 2)->first();
+        dd($addressInserted);
+
+        DB::table('customers')->insertGetId(
+            ['id_lang' => 1],
+            ['id_address' => $addressInserted->id_address],
+            ['is_guest' => 0],
+            ['first_name' => $request->name],
+            ['last_name' => $request->last_name],
+            ['email' => $request->email],
+            ['phone' => $request->phone]
+          );
+
+      //     return redirect()->back()->with('Datos del cliente guardados');
+      // }else{
+      //     return redirect()->back()->with('error', 'Faltan datos del cliente');
+      // }
 
     }
 
--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -9,62 +9,63 @@
       </div>
       <div class="modal-body">
         <form class="form-group" action="{{route('customer.store')}}" method="post">
+          @csrf
           <div class="row">
             <div class="col-md-6">
               <label for="customer_name">First Name</label>
-              <input type="text" class="form-control" id="customer_name" value="" maxlength="255" required>
+              <input type="text" class="form-control" name="name" id="customer_name" value="" maxlength="255" required>
             </div>
             <div class="col-md-6">
               <label for="customer_last_name">Last Name</label>
-              <input type="text" class="form-control" id="customer_last_name" value="" maxlength="255" required>
+              <input type="text" class="form-control" name="last_name" id="customer_last_name" value="" maxlength="255" required>
             </div>
           </div>
           <div class="form-row">
             <div class="form-group col-md-6">
               <label for="customer_phone">Phone</label>
-              <input type="number" class="form-control" id="customer_phone" min="600000000" max="999999999" required>
+              <input type="number" class="form-control" name="phone" id="customer_phone" min="600000000" max="999999999" required>
             </div>
             <div class="form-group col-md-6">
               <label for="customer_password">Password</label>
-              <input type="password" class="form-control" id="customer_password" required>
+              <input type="password" class="form-control" name="password" id="customer_password" required>
             </div>
           </div>
           <div class="form-row">
             <div class="form-group col-md-12">
               <label for="customer_email">Email</label>
-              <input type="email" class="form-control" id="customer_email" maxlength="150" required>
+              <input type="email" class="form-control" name="email" id="customer_email" maxlength="150" required>
             </div>
           </div>
           <div class="form-group">
             <label for="customer_address">Address</label>
-            <input type="text" class="form-control" id="customer_address" placeholder="1234 Main St" maxlength="128" required>
+            <input type="text" class="form-control" name="address" id="customer_address" placeholder="1234 Main St" maxlength="128" required>
           </div>
-          <div class="form-group">
+          {{-- <div class="form-group">
             <label for="customer_address_aux">Address 2</label>
-            <input type="text" class="form-control" id="customer_address_aux" placeholder="Apartment, studio, or floor" maxlength="128">
-          </div>
+            <input type="text" class="form-control" name="address_aux" id="customer_address_aux" placeholder="Apartment, studio, or floor" maxlength="128">
+          </div> --}}
           <div class="form-row">
             <div class="form-group col-md-4">
               <label for="customer_country">Country</label>
-              <select id="customer_country" class="form-control">
+              <select name="country" id="customer_country" class="form-control">
                 <option selected>Choose...</option>
                 @foreach ($countries as $country)
-                  <option>{{$country->name}}</option>
+                  <option value="{{$country->id_country}}">{{$country->name}}</option>
                 @endforeach
               </select>
             </div>
             <div class="form-group col-md-4">
               <label for="customer_state">State</label>
-              <select id="customer_state" class="form-control">
+              <select name="state" id="customer_state" class="form-control">
                 <option selected>Choose...</option>
                 @foreach ($states as $state)
-                  <option>{{$state->name}}</option>
+                  <option value="{{$state->id_state}}">{{$state->name}}</option>
                 @endforeach
               </select>
             </div>
             <div class="form-group col-md-4">
               <label for="customer_zip">Zip</label>
-              <input type="number" class="form-control" id="customer_zip" max="99999" required>
+              <input type="number" class="form-control" name="zip" id="customer_zip" max="99999" required>
             </div>
           </div>
           <div class="form-group">
