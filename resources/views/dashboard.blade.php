47b864bdd99bab00a515a3c464d30599215f2383
rafa <rjuanferragut@gmail.com>
2020-02-13 14:36:45 +0100

    arreglos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,30 +3,7 @@
 <div class="container pl-5">
   <div>
     <h1 class="mt-5 text-center"> Bienvenido al panel del administrador</h1>
-    <p>cambiar nombres de los archivos escrito en la agenda</p>
   </div>
-  <div id="adminriders" class="">
-    <p>Aqui va admin PANEL riders INFO</p>
-  </div>
-  <div id="admincustomers" class="">
-    <p>Aqui va admin PANEL customers INFO</p>
-
-  </div>
-  <div id="adminsuppliers" class="">
-    <p>Aqui va admin PANEL suppliers INFO</p>
-
-  </div>
-  <div id="adminproducts" class="">
-    <p>Aqui va admin PANEL products INFO</p>
-
-  </div>
-  <div id="adminorders" class="">
-    <p>Aqui va admin PANEL orders INFO</p>
-
-  </div>
-  <div id="adminemployees" class="">
-    <p>Aqui va admin PANEL employees INFO</p>
-
-  </div>
+  <!-- información general de las categorias -->
 </div>
 @endsection
